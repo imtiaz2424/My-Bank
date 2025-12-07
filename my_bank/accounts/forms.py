d414cbdf26b25b0ece02b8d0d21956ad0fa88f21
unknown <your_email@example.com>
2025-12-07 07:10:24 +0600

    Creating Base HTML

--- my_bank/accounts/forms.py
+++ my_bank/accounts/forms.py
@@ -47,3 +47,16 @@ class UserRegistrationForm(UserCreationForm):
                 gender = gender                
             )
         return our_user
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class' : (
+                    'appearance-none block w-full bg-gray-200'
+                    'text-gray-700 border border-gray-200 rounded'
+                    'py-3 px-4 leading-tight focus:outline-none'
+                    'focus:bg-white focus:border-gray-600'
+                )
+            })
